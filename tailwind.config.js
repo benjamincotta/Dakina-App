89ec59c5688fa111e7037a141788f5d355a909d6
Benjamin Cotta <74515337+benjamincotta@users.noreply.github.com>
2022-07-12 18:25:20 +0100

    color palette added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ['./public/**/*.html'],
   theme: {
     extend: {},
+    colors: {
+      darker_blue: '#005a8a',
+      dark_blue: '#01CCC1',
+      turquoise: '#27F4E0',
+      ghost_white: '#FAFAFF',
+      pink: '#E56399',
+    },
   },
   plugins: [],
 }
