e8f08bc49455a44cc49660d88a9fc29c4e3e710e
Benjamin Cotta <74515337+benjamincotta@users.noreply.github.com>
2022-07-12 19:32:09 +0100

    built out header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 module.exports = {
   content: ['./public/**/*.html'],
   theme: {
-    extend: {},
-    colors: {
-      darker_blue: '#005a8a',
-      dark_blue: '#01CCC1',
-      turquoise: '#27F4E0',
-      ghost_white: '#FAFAFF',
-      pink: '#E56399',
+    extend: {
+      fontFamily: {
+        'brand-font': ['Rochester', 'cursive'],
+        'text-font': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        background: '#313479',
+        dark_blue: '#01CCC1',
+        turquoise: '#27F4E0',
+        ghost_white: '#FAFAFF',
+        pink: '#E56399',
+        white: '#FFFFF',
+      },
     },
   },
   plugins: [],
